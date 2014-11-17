bc1ae089a1c334eaeb56b4670cda6de5b01a6133
Bessem Slim <bessem.slim@qbranch.se>
2014-11-17 10:38:28 +0100

    Init

---
+++ WebApp17/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp17.MvcApplication" Language="C#" %>
---
+++ WebApp17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp17.Startup))]
+namespace WebApp17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
